83364ba0b6866a07c3c9285335da14d1439893ee
unknown <ajaydevofficial@gmail.com>
2019-03-16 22:42:33 +0530

    Final Commit

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -3,22 +3,46 @@ google.charts.setOnLoadCallback(drawVisualization);
 function drawVisualization() {
        // Some raw data (not necessarily accurate)
        var data = google.visualization.arrayToDataTable([
-         ['Month', 'Bolivia', 'Ecuador', 'Madagascar', 'Papua New Guinea', 'Rwanda', 'Average'],
-         ['2004/05',  165,      938,         522,             998,           450,      614.6],
-         ['2005/06',  135,      1120,        599,             1268,          288,      682],
-         ['2006/07',  157,      1167,        587,             807,           397,      623],
-         ['2007/08',  139,      1110,        615,             968,           215,      609.4],
-         ['2008/09',  136,      691,         629,             1026,          366,      569.6]
+         ['Year', 'Total-Walk-ins', 'Registrations', 'Number of Events(Including Shows and Pre-events)'],
+         ['2012',  400,                 250,                25,],
+         ['2016',  700,                600,                40, ],
+         ['2017',  900,                750,                60, ],
+         ['2018',  1000,                850,                70,],
        ]);
 
        var options = {
-         title : 'Monthly Coffee Production by Country',
-         vAxis: {title: 'Cups'},
-         hAxis: {title: 'Month'},
+         vAxis: {title: 'Numbers'},
+         hAxis: {title: 'Year'},
          seriesType: 'bars',
-         series: {5: {type: 'line'}}
+         series: {5: {type: 'line'}},
+         colors: ['#9e34db', '#3598db', '#2ecc71']
        };
-
        var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
        chart.draw(data, options);
 }
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawVisualization2);
+function drawVisualization2() {
+       // Some raw data (not necessarily accurate)
+       var data = google.visualization.arrayToDataTable([
+         ['Year', 'Prize Money(In Lakhs)'],
+         ['2012', 1.5],
+         ['2016', 3],
+         ['2017', 5],
+         ['2018', 4.6],
+       ]);
+
+       var options = {
+         vAxis: {title: 'Prize-Money(in Lakhs)'},
+         hAxis: {title: 'Year'},
+         seriesType: 'bars',
+         series: {5: {type: 'line'}},
+         colors: ['#db3463']
+       };
+       var chart = new google.visualization.ComboChart(document.getElementById('chart_div_2'));
+       chart.draw(data, options);
+}
+$(window).resize(function(){
+  drawVisualization();
+  drawVisualization2();
+});
